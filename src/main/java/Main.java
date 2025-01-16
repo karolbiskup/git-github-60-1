dc12380a392fd3f7124dee1a644f79b43fb745dc
Karol Biskup <karolbiskup1234@gmail.com>
2025-01-16 17:12:59 +0100

    Zmiana w drugim repo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ public calss Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
+
+    private void displayText() {
+        System.out.println("zmiana w drugim repo")
+    }
 }
