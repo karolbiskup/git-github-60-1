fadc2479cd254228c36c9ea3c4b7d3d26544e726
Karol Biskup <karolbiskup1234@gmail.com>
2025-01-16 17:08:17 +0100

    Zmiana Hello World

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public calss Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Poland");
     }
 
     private void displayText() {
